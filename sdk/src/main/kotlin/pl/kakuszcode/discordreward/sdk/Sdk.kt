2bc1722b0d6bb8179fc242db3390c4bf52a72a3a
robalmeister <65306693+robalmeister@users.noreply.github.com>
2022-12-21 20:44:25 +0100

    Update Sdk.kt

--- sdk/src/main/kotlin/pl/kakuszcode/discordreward/sdk/Sdk.kt
+++ sdk/src/main/kotlin/pl/kakuszcode/discordreward/sdk/Sdk.kt
@@ -4,6 +4,7 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
 import io.ktor.http.*
